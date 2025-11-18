c2e2b2d9a31d9a9cb4db2408596bf29f848fecc4
jbj338033 <jbj338033@gmail.com>
2025-11-18 11:08:40 +0900

    style: add missing semicolons in config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         primary: {
           DEFAULT: '#FF6B6B',
           hover: '#FF5252',
-          light: '#FFE3E3'
+          light: '#FFE3E3',
         },
         secondary: {
           DEFAULT: '#4ECDC4',
           hover: '#45B8B0',
-          light: '#E0F7F6'
-        }
-      }
+          light: '#E0F7F6',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
